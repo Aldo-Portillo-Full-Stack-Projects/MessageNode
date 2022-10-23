da68ed9db9957d5f515bc9e9b6a6e4b0919c9a0e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-23 17:56:19 -0500

    Connect MongoDB  database to app.js

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const { response } = require('express');
 const { render } = require('ejs');
 const app = express();
-//Register view engine\
+
+
+const dbURI = 'mongodb+srv://admin:toorMB@messageboard.9exeze1.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+
+
 app.set('view engine', 'ejs'); 
 
-//middleware and static files
 app.use(express.static('public'))
 
-//Takes form data
 app.use(express.urlencoded({extended:true}));
 
-//Third party middleware npm morgan
 app.use(morgan('dev'))
 
-app.listen(3000)
 
 
 
