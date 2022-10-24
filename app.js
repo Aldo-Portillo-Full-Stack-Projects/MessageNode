66c3d5a5eff53e206f709699c47d26e9d1b21cfd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-24 16:50:55 -0500

    Organize code - getting an empty array from database

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const { response } = require('express');
-const { render } = require('ejs');
-const app = express();
+  const morgan = require('morgan')
+  
+  
+  const mongoose = require('mongoose');
+  
+  const Message = require('./models/message')
+  const { response } = require('express');
+  const { render } = require('ejs');
+  
+  // express app
+  const app = express();
+  
+  // connect to mongodb & listen for requests
+  const dbURI = "mongodb+srv://admin:toorMB@messageboard.9exeze1.mongodb.net/?retryWrites=true&w=majority";
+  
+  mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
+  
+  //Register view engine\
+  app.set('view engine', 'ejs'); 
+  
+  //middleware and static files
+  app.use(express.static('public'))
+  
+  //Takes form data
+  app.use(express.urlencoded({extended:true}));
+  
+  //Third party middleware npm morgan
+  app.use(morgan('dev'))
 
-const Message = require('./models/message')
 
-app.set('view engine', 'ejs'); 
-
-app.use(express.static('public'))
-
-app.use(express.urlencoded({extended:true}));
-
-app.use(morgan('dev'))
-
-//app.listen(3000)
-
-const dbURI = 'mongodb+srv://admin:toorMB@messageboard.9exeze1.mongodb.net/?retryWrites=true&w=majority'
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
-
-
+//Get Requests
 app.get('/', (req, res) => {
 
     Message.find().sort({createdAt: -1})
         .then((result) => {
+            console.log(result)
             res.render('index', {title: 'Home', messages: result})
         })
         .catch((err)=> {
@@ -35,25 +44,31 @@ app.get('/', (req, res) => {
         })
 });
 
-app.post('/new', (req, res) => {
-  const message = new Message(req.body)
-
-  message.save()
-      .then((result) => {
-        console.log(result)
-          res.redirect('/')
-      })
-      .catch((err)=> {
-          console.log(err)
-      })
-})
-
 app.get('/new', (req, res) => {
 
-        res.render('new', { title: 'New'})
+  res.render('new', { title: 'New'})
 })
 
+//Post Requests
 
+// app.post('/new', (req, res) => {
+//   const message = new Message(req.body)
+
+//   message.save()
+//       .then((result) => {
+//         console.log(result)
+//           res.redirect('/')
+//       })
+//       .catch((err)=> {
+//           console.log(err)
+//       })
+// })
+
+
+
+
+
+//404 Status Code
 app.use((req, res)=> {
   res.status(404).render('404', { title: 'Error'})
 })
